7663fa378869a099afd656f44bb789ff2e8386fd
arifphero <ahammadarif232@gmail.com>
2022-05-08 16:33:12 +0600

    first api created

--- index.js
+++ index.js
@@ -10,6 +10,23 @@ app.use(express());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mp2nv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function run() {
+  try {
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("hikero warehouse");
+});
+
 app.listen(port, () => {
   console.log("listening to port", port);
 });
