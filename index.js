2d75db45d4f39318a65ccb9037c4175328bedfa2
arifphero <ahammadarif232@gmail.com>
2022-05-08 17:07:26 +0600

    delete quantity

--- index.js
+++ index.js
@@ -19,14 +19,51 @@ async function run() {
   try {
     await client.connect();
     const database = client.db("travelTourism");
-    const servicesCollection = database.collection("services");
+    const inventoryCollection = database.collection("inventory");
     const bookingCollection = database.collection("booking");
 
     //GET API
-    app.get("/services", async (req, res) => {
-      const cursor = servicesCollection.find({});
-      const services = await cursor.toArray();
-      res.send(services);
+    app.get("/inventory", async (req, res) => {
+      const cursor = inventoryCollection.find({});
+      const inventory = await cursor.toArray();
+      res.send(inventory);
+    });
+
+    app.get("/inventory/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await inventoryCollection.findOne(query);
+      res.json(service);
+    });
+
+    //POST API
+    app.post("/inventory", async (req, res) => {
+      const service = req.body;
+      console.log("hit the post api", service);
+
+      const result = await inventoryCollection.insertOne(service);
+      console.log(result);
+      res.json(result);
+    });
+    //DELETE API
+    app.delete("/booking/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await bookingCollection.deleteOne(query);
+      res.json(result);
+    });
+
+    //Add Booking API
+    app.post("/booking", async (req, res) => {
+      const booking = req.body;
+      const result = await bookingCollection.insertOne(booking);
+      res.json(result);
+    });
+    // Get Booking Data
+    app.get("/booking", async (req, res) => {
+      const cursor = bookingCollection.find({});
+      const booking = await cursor.toArray();
+      res.send(booking);
     });
   } finally {
     // await client.close();
