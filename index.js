b798dc0e1bbd2e118ac95df0df7cd1e91dcd84ec
arifphero <ahammadarif232@gmail.com>
2022-06-06 16:27:28 +0600

    handle error

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
+/* error */
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hf6om.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
